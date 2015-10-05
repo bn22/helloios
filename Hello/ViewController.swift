7e0fe868a878bca23808e88fc5a58a44fefb3cbe
Your Name <you@example.com>
2015-10-05 15:13:46 -0700

    first

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -2,25 +2,24 @@
 //  ViewController.swift
 //  Hello
 //
-//  Created by iGuest on 10/1/15.
-//  Copyright (c) 2015 iGuest. All rights reserved.
+//  Created by Bruce Ng on 10/5/15.
+//  Copyright © 2015 Bruce Ng. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var mainText: UILabel!
-    
-    @IBAction func labelChanger(sender: UIButton)
-    {
-        let buttonText = sender.currentTitle!
-        
-        if (buttonText.isEmpty) {
-            println("It has a value")
-        }
-        mainText.text = buttonText
-        println("Button Text = \(buttonText)")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
 }
 
